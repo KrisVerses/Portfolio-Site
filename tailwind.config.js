a0f297d5e03d6fa4b76e24452ca6543e33a0d911
Kris Norful <krisnorful@gmail.com>
2025-01-28 17:09:59 -0600

    navbar work

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: true, // Ensures Preflight is enabled
+  },
+};
